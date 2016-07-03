316e0f6e3ae8381e6f3a26417f77f5da3a2503e5
Lajos Katona <lajos.katona@ericsson.com>
2016-07-03 21:22:53 +0200

    The fourth theorem: counting
    Change-Id: If0abb28deedb65c45633426d98881d8ee8256f85

---
+++ programmingtheorems/python/theorem_of_counting.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+# Copyright Lajos Katona
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+
+def count_brute(mylist, t):
+    counter = 0
+    for l in mylist:
+        if l == t:
+            counter += 1
+    return counter
+
+
+def counter_pythonic(mylist, t):
+    return mylist.count(t)
+
+
+if __name__ == '__main__':
+    mylist = [1, 2, 3, 4, 5, 4, 3, 2]
+    print(count_brute(mylist, 4))
+    print(counter_pythonic(mylist, 4))
