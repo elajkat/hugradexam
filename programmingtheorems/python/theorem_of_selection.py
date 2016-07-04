2b6d24caa29ae22bf15663df66a2c4bac2bbfc0d
Lajos Katona <lajos.katona@ericsson.com>
2016-07-04 22:12:32 +0200

    The sixth theorem: the min and max selection
    Change-Id: I83f1f0c066f182fa025dc0ae9790f96451a6ca2c

--- programmingtheorems/python/theorem_of_selection.py
+++ programmingtheorems/python/theorem_of_selection.py
@@ -26,7 +26,38 @@ def selection_pythonic(mlist, t):
     return mlist.index(t)
 
 
+def select_max_brute(mlist):
+    max = mlist[0]
+    max_index = -1
+    for i, l in enumerate(mlist):
+        if l > max:
+            max_index = i
+    return max_index
+
+
+def select_max_pythonic(mlist):
+    return mlist.index(max(mlist))
+
+
+def select_min_brute(mlist):
+    min = mlist[0]
+    min_index = -1
+    for i, l in enumerate(mlist):
+        if l < min:
+            min_index = i
+    return min_index
+
+
+def select_min_pythonic(mylist):
+    return mylist.index(min(mylist))
+
+
 if __name__ == '__main__':
     mylist = [1, 2, 3, 4, 5]
+    mylist2 = [1, 2, 3, 2, 1, 10, 9, 8, 11, -1]
     print(selection_brute(mylist, 4))
     print(selection_pythonic(mylist, 4))
+    print(select_max_brute(mylist2))
+    print(select_max_pythonic(mylist2))
+    print(select_min_brute(mylist2))
+    print(select_min_pythonic(mylist2))
